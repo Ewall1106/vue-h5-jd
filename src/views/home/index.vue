392e60299d60e41e36048ddb93e516e81d404c13
xiongwei <>
2022-02-20 22:58:49 +0800

    feat: add SecondGoodsKill

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,28 @@
 <template>
-  <div>
+  <div class="home">
     <ImageSwipe />
     <NavSwipe />
+    <SecondGoodsKill />
   </div>
 </template>
 
 <script>
 import ImageSwipe from "@/components/ImageSwipe";
 import NavSwipe from "@/components/NavSwipe";
+import SecondGoodsKill from "@/components/SecondGoodsKill";
 
 export default {
   name: "Home",
   components: {
     ImageSwipe,
     NavSwipe,
+    SecondGoodsKill,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  background: #efefef;
+}
+</style>
