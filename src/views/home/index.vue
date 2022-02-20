0ceafa19a9fb11df451e71f362bb605a643daf68
xiongwei <>
2022-02-20 12:51:02 +0800

    feat: add NavSwipe

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,19 @@
 <template>
-  <div><ImageSwipe /></div>
+  <div>
+    <ImageSwipe />
+    <NavSwipe />
+  </div>
 </template>
 
 <script>
 import ImageSwipe from "@/components/ImageSwipe";
+import NavSwipe from "@/components/NavSwipe";
+
 export default {
   name: "Home",
   components: {
     ImageSwipe,
+    NavSwipe,
   },
 };
 </script>
